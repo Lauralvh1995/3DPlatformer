5525185c3c7e191e74939ccc68015af385df301d
Helden,Laura L.H.A. van <l.vanhelden@student.fontys.nl>
2020-02-03 17:13:31 +0100

    Designing UI a bit

---
+++ Assets/Scripts/ResourceBarController.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceBarController : MonoBehaviour
+{
+    public GameManager manager;
+
+    public Slider HPslider;
+    public Slider MPslider;
+
+    private void Start()
+    {
+        manager = GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        HPslider.value = manager.player.getHP() / (float)manager.player.getMHP();
+        MPslider.value = manager.player.getMP() / (float)manager.player.getMMP();
+    }
+}
